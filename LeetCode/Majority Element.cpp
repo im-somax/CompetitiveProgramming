d00bd6cd8f44898b3c6f5f33929857fd1f5b6abe
Somesh Patel <56200611+im-somax@users.noreply.github.com>
2021-07-13 17:42:44 +0530

    CC

---
+++ LeetCode/Majority Element.cpp
@@ -0,0 +1,13 @@
+int majorityElement(vector<int>& nums) {
+        int count = 1;
+        int major = nums[0];
+        for (int i = 1; i < nums.size(); i ++) {
+            if (nums[i] == major) count ++;
+            else count --;
+            if (count == 0) {
+                major = nums[i];
+                count = 1;
+            }
+        }
+        return major;
+    }
